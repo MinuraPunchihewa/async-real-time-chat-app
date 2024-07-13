7418596343a1d462b93f26bdd79b3f6abedc7491
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-07-13 20:42:02 +0530

    refactored db module to use redis-py

--- db.py
+++ db.py
@@ -5,23 +5,26 @@ from redis import asyncio as aioredis
 class ChatDB:
     def __init__(self, room_name):
         self.room_name = room_name
+        self.pool = None
+        self.client = None
 
     async def connect(self):
-        self.redis = await aioredis.create_redis('redis://localhost')
-        await self.redis.set('room_name', self.room_name)
+        self.pool = aioredis.ConnectionPool.from_url("redis://localhost")
+        self.client = aioredis.Redis(connection_pool=self.pool)
+        await self.client.set('room_name', self.room_name)
 
     async def disconnect(self):
-        await self.redis.flushall()
-        self.redis.close()
+        await self.client.flushall()
+        self.client.close()
 
     async def add_message(self, message):
-        room_name = await self.redis.get('room_name')
-        await self.redis.rpush(
+        room_name = await self.client.get('room_name')
+        await self.client.rpush(
             room_name,
             json.dumps(message)
         )
 
     async def get_messages(self):
-        room_name = await self.redis.get('room_name')
-        messages = await self.redis.lrange(room_name, 0, -1)
-        return [json.loads(message) for message in messages]
+        room_name = await self.client.get('room_name')
+        messages = await self.client.lrange(room_name, 0, -1)
+        return [eval(json.loads(message)) for message in messages]
